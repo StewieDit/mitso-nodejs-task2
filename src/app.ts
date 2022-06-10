9c1b9c3706fbfa83e0c864fff03994d41ba20d7a
StewieDit <81706957+StewieDit@users.noreply.github.com>
2022-06-10 16:18:01 +0300

    task 3 complete

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import swaggerUI from 'swagger-ui-express';
+import path from 'path';
+import YAML from 'yamljs';
+import clientRouter from './resources/clients/client.router';
+import productRouter from './resources/products/product.router';
+import orderRouter from './resources/orders/order.router';
+
+const app = express();
+const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
+
+app.use(express.json());
+
+app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
+app.use('/', (req, res, next) => {
+  if (req.originalUrl === '/') {
+    res.send('Service is running!');
+    return;
+  }
+  next();
+});
+
+app.use('/clients', clientRouter);
+app.use('/products', productRouter);
+app.use('/orders', orderRouter);
+
+export default app;
